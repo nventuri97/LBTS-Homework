31ba24d8b21827eff81b305213fa3253de2d8185
Nicola Venturi <n.venturi3@studenti.unipi.it>
2024-05-03 15:42:06 +0200

    Update to interpreter structure, handling of string, trust block, include and execute

--- homework_1/lib/interpreter.ml
+++ homework_1/lib/interpreter.ml
@@ -6,18 +6,19 @@ let rec eval (e : expr) (env : value env) (t: bool) (stack : pstack): value =
     match e with
     | CstI i -> Int i
     | CstB b -> Bool (if b then true else false)
-    | Var (x, t) -> Value (lookup env x, taint_lookup env x)
+    | CstString s -> String s
+    | Var (x, _) -> Value (lookup env x, taint_lookup env x)
     | Let (x, eRhs, letBody) ->
         let xVal = eval eRhs env t stack in
         let letEnv = (x, xVal, t) :: env in
         eval letBody letEnv t stack
     | SecLet (x, eRhs, secSet, letBody) ->
-  (* xVal is evaluated in the current stack *)
+        (* xVal is evaluated in the current stack *)
         let xVal = eval eRhs env t stack in
         let letEnv = (x, xVal, t) :: env in
         let letStack = Grant secSet :: stack in
   
-  (* letBody is evaluated in the updated stack *)
+        (* letBody is evaluated in the updated stack *)
         eval letBody letEnv t letStack
     | Prim (ope, e1, e2) -> (
         match (eval e1 env t stack, eval e2 env t stack) with
@@ -37,23 +38,24 @@ let rec eval (e : expr) (env : value env) (t: bool) (stack : pstack): value =
         match eval e1 env t stack with
         | Value (Bool true, t1) -> eval e2 env t1 stack
         | Value (Bool false, t1) -> eval e3 env t1 stack
-        | Value (_, t1) -> failwith "If condition must be a boolean"
+        | Value (_, _) -> failwith "If condition must be a boolean"
         | _ -> failwith "Improper use in If condition"
       )
     | Fun (x, fBody, secSet) -> Value (Closure (x, fBody, secSet, env, t), t)
-  (*This part of the call to a function must be checked, here we have the error*)
+    (*This part of the call to a function must be checked, here we have the error*)
     | Call (eFun, eArg) -> (
         let fClosure = eval eFun env t stack in
         match fClosure with
         | Closure (x, fBody, secSet, fDeclEnv, t) ->
-  (* xVal is evaluated in the current stack *)
+            (* xVal is evaluated in the current stack *)
             let xVal = eval eArg env t stack in
             let fBodyEnv = (x, xVal, t) :: fDeclEnv in
             let fBodyStack = Grant secSet :: stack in
   
-  (* fBody is evaluated in the updated stack *)
+          (* fBody is evaluated in the updated stack *)
             eval fBody fBodyEnv t fBodyStack
         | _ -> failwith "eval Call: not a function")
+    (* The stuff below are useless for us*)
     | DemandPermission p -> Int (stackInspection inspect stack p)
     | OnPermission (p, e) ->
         if eval (DemandPermission p) env t stack = Int 1 then eval e env t stack
@@ -76,5 +78,13 @@ let rec eval (e : expr) (env : value env) (t: bool) (stack : pstack): value =
           = Int 1
         then eval e env t stack (* do write *)
         else eval (Abort ("No Write Permission for " ^ f)) env t stack
+    (*Useful for us*)
     | Abort msg -> failwith msg
     | GetInput(e) -> eval e env true stack
+    | TrustBlock (_, _) -> failwith "Not yet implemented"
+    | Include (_, _, _) -> failwith "Not yet implemented"
+    | Execute (e1, e2) ->
+      match eval e1 env t stack with
+      | TrustBlock (_,_) -> failwith "Not yet implemented"
+      | Include (_, _, _) -> failwith "Not yet implemented"
+      | _ -> failwith "Impossible to execute"
