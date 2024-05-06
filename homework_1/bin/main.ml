d972d97cb61258e98fcdcc6615943e5b166ef897
Nicola Venturi <n.venturi3@studenti.unipi.it>
2024-05-06 10:05:44 +0200

    Partial merge from paolof -- main

--- homework_1/bin/main.ml
+++ homework_1/bin/main.ml
@@ -1,44 +1,72 @@
-open Interpreter
-open Security
+open homework_1.Interpreter
+open homework_1.Security
+(*
+let execWithFailure test env stack =
+  let value = try eval test env stack with Failure _ -> Int 1 in
+  assert (value = Int 1)
 
-let execWithFailure test=
-  let value = try eval test with Failure _ -> Int 1 in
-  assert (value = Int 1);;
+(*
+  For testing purpose: test if the evaluation does not fail
+*)
+let execWithoutFailure test env stack =
+  let value = try eval test env stack with Failure _ -> Int 0 in
+  assert (value <> Int 0)
 
-let execWithoutFailure test=
-  let value = try eval test with Failure _ -> Int 0 in
-  assert (value <> Int 0);;
 
-let test_1 = Trust("Mycode","Felix");;
-let test_2= Include("Mycode", "Felix", Secret, (*Operazione1 segreta da aggiungere*));;
-execWithoutFailure test_2;; (*da capire come collegare il risultato della eval a exec without e with failure*)
-let test_3= Include("Mycode", "", Public, (*Operazione2 publica da aggiungere*));;
-execWithoutFailure test_3;; (*non deve fallire*)
+  let mycode= trust{
+    let secret x=0
+    let sum p1 p2= p1+p2 
+    handle sum
+}
+*)
 
-let test_4= Include("Mycode", "", Secret, (*Operazione, non è importante quale visto che fallisce*));;
-execWithFailure test_4;; (*DEVE fallire perchè stiamo creando un segreto senza mettere la password*)
+TrustBlock(
+  "MyCode",
+  Let_Secret("x", CstI 0),
+  Let_Public(
+    "sum",
+    Fun(
+      "p1", "p2",
+      Prim("+", Var("p1"), Var("p2"))
+    )
+  ),
+  Handle("sum"),
+EndTrustBlock
+);
+(*
+  let mycode1= trust{
+    let secret x=0
+    handle x
+} 
+Error: you cant handle a secret information
+*)
+TrustBlock(
+  "MyCode1",
+  Let_Secret("x", CstI 0),
+  Handle("x"),
+  EndTrustBlock
+);
 
-let test_5= Include("Mycode", "Sad", Secret, (*Operazione segreta da aggiungere*));;
-execWithFailure test_5;; (*Deve fallire perchè la password è sbagliata*)
+(*
+let inc_fun = include{
+  let x=2
+  let y=3
+  let mult p1 p2= p1*p2
+}
+execute(inc_fun.mult, 3, 2);
+*)
+Include(
+  "inc_fun",
+  Let("x", CstI 2),
+  Let("y", CstI 3),
+  Let("mult",
+    Fun(
+      "p1", "p2", 
+      Prim("*", Var "p1", Var"p2"))
+    )
+Execute("inc_fun.mult", CstI 3, CstI 4);
+)
 
-let test_6= Execute("Mycode", "Felix", (*Operazione1*));;
-execWithoutFailure test_6;; (*non deve fallire*)
-let test_7= Execute("Mycode", "", (*Operazione2*));;
-execWithoutFailure test_7;; (*non deve fallire perchè è pubblica *)
-let test_8= Execute("Mycode", "", (*Operazione1*));;
-execWithFailure test_8;; (*deve fallire perchè l'operazione1 è dichiarata segreta e non ho messo la password *)
-let test_9= Execute("Mycode", "Sad", (*Operazione1*));;
-execWithFailure test_9;; (* deve fallire per password sbagliata*)
-let test_10= Execute("Mycode1", "", (*Operazione2*));;
-execWithFailure test_10;; (*deve fallire perchè non esiste un trust mycode1*)
-
-let test_11= Trust("Mycode", "Rand");;
-execWithFailure test_11;; (* deve fallire perchè esiste già un trust con quel nome*)
-
-let t1 =
-  let secret (x,y) =
-    (*Calculate the position, in an hidden way*)
-  in Trust("MyPosition", t1)
 
 (*Parte della dynamic tainted analysis*)
 
