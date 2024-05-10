1bcf31d85536d8cf300b50ad06747e2ec2725611
yesiam2306 <56731223+yesiam2306@users.noreply.github.com>
2024-05-11 00:12:56 +0200

    Update env.ml
    Nessuna modifica. rimesso in pari con il branch principale

--- homework_1/lib/env.ml
+++ homework_1/lib/env.ml
@@ -1,3 +1,4 @@
+
 (*
   Variable identifiers are strings
 *)
@@ -7,8 +8,11 @@ type ide = string
   An environment is a map from identifier to a value (what the identifier is bound to).
   For simplicity we represent the environment as an association list, i.e., a list of pair (identifier, data).
 *)
+(* type 'v env = (ide * 'v * bool) list *)
 type 'v env = (ide * 'v) list
 
+(*To extend an environment*)
+let extend (e : 'v env) (id : ide) (v : 'v) : 'v env = (id, v) :: e
 (*
   Given an environment {env} and an identifier {x} it returns the data {x} is bound to.
   If there is no binding, it raises an exception.
@@ -17,3 +21,8 @@ let rec lookup env x =
   match env with
   | [] -> failwith (x ^ " not found")
   | (y, v) :: r -> if x = y then v else lookup r x
+
+let rec taint_lookup env x=
+  match env with
+  | [] -> failwith (x ^ " not found")
+  | (y, _, t)::r -> if x = y then t else taint_lookup r x 
