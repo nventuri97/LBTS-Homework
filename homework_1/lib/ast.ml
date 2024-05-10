ad871bef7181087b11c11bd3d7e3f65b3db4858f
yesiam2306 <56731223+yesiam2306@users.noreply.github.com>
2024-05-11 00:11:04 +0200

    Update ast.ml
    modificati costruttori di Include e Execute
    aggiunto costruttore ClosureInclude

--- homework_1/lib/ast.ml
+++ homework_1/lib/ast.ml
@@ -1,39 +1,39 @@
-open Security
-open Env
 
 type expr =
   | CstI of int
   | CstB of bool
+  | CstString of string
+  (* | Var of ide * bool *)
   | Var of ide
-  | Let of ide * expr * expr
-  (* SecLet evaluates the expressions pushing the given pdomain on top of the stack *)
-  | SecLet of ide * expr * pdomain * expr
+  | Assign of ide * expr (*let x=...*)
+  | Let of ide * expr * expr (*let x= x in...*)
   | Prim of ide * expr * expr
   | If of expr * expr * expr
   (* Lambda: parameters, body and permission domain *)
-  | Fun of ide * expr * pdomain
+  | Fun of ide * expr 
   | Call of expr * expr
-  (* Return true iff that permission is allowed *)
-  | DemandPermission of permission
-  (* Evaluates the expression iff DemandPermission is true otherwise return false (Int 0) *)
-  | OnPermission of permission * expr
-  (* Aborts if permission is not enabled *)
-  | CheckPermission of permission
-  (* Evaluates the expression with the permission enabled *)
-  | Enable of permission * expr
-  (* Evaluates the expression with the permission disabled *)
-  | Disable of permission * expr
-  (* Evaluates the expression pushing the secAction on top of the stack *)
-  | SecBlock of secAction * expr
-  (* Reads a file iff is allowed otherwise aborts *)
-  | ReadFile of string
-    (* Send and evaluates expr to a file iff is allowed otherwise aborts *)
-  | SendFile of expr * string
   | Abort of string
+  (*This part of the code is added in order to test the DTA*)
+  | GetInput of expr    (*functions that takes input, taint source*)
+  | TrustBlock of trustContent
+  | Include of expr
+  | Execute of expr
+and trustContent =
+  | LetSecret of ide * expr * trustContent
+  | LetPublic of ide * expr * trustContent
+  | Handle of ide * trustContent
+  | EndTrustBlock
 
 (*
   A runtime value is an integer or a function closure
   Boolean are encoded as integers.
 *)
-type value = Int of int | Closure of ide * expr * pdomain * value env
-(* In a closuer is saved also the pdomain *)
+type value = 
+  | Int of int
+  | Bool of bool
+  | String of string 
+  (* | Value of value * bool *)
+  (* | Closure of ide * expr * value env * bool *)
+  | Closure of ide * expr * value env
+  | ClosureInclude of expr * value env
+  | Block of string
